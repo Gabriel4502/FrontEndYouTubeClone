a48c8d2344a6e15b3651a87d88ff2ba4fab86a8f
Gabriel Messias <gabrielmessias3@hotmail.com>
2023-04-10 20:30:46 -0300

    Header parte visual

---
+++ src/components/header/index.tsx
@@ -0,0 +1,59 @@
+import { 
+    Container,
+    LogoContainer, 
+    ButtonContainer, 
+    ButtonIcon,
+    SearchContainer,
+    SearchInputContainer,
+    SearchInput,
+    SearchButton,
+    HeaderButton } from "./style";
+import HamburguerIcon from '../../assets/hamburger.png';
+import Logo from '../../assets/YouTube-Logo.png';
+import SearchIcon from '../../assets/search.png';
+import MicIcon from '../../assets/microfone-gravador.png'
+import VideoIcon from '../../assets/video.png'
+import SinoIcon from '../../assets/sino.png'
+
+function Header(){
+    return (
+        <Container>
+            <LogoContainer>
+                <ButtonContainer margin='0 10px 0 0' >
+                    <ButtonIcon  alt ="" src={HamburguerIcon}/>
+                </ButtonContainer>
+                <img style={{cursor: 'pointer', width: '100px'}} 
+                alt ='Página inicial do YouTube'
+                src={Logo}
+                />
+            </LogoContainer>
+
+        <SearchContainer>
+            <SearchInputContainer>
+                <SearchInput placeholder="Pesquisar" />
+            </SearchInputContainer>
+            <SearchButton>
+                <ButtonIcon alt="" src={SearchIcon} />
+            </SearchButton>
+            <ButtonContainer margin='0 0 0 10px' >
+                <ButtonIcon  alt="" src={MicIcon} />
+            </ButtonContainer>
+        </SearchContainer>
+
+        <HeaderButton>
+            <ButtonContainer>
+                <ButtonIcon alt=""  src={VideoIcon} />
+            </ButtonContainer>
+            <ButtonContainer>
+                <ButtonIcon alt="" src= {SinoIcon} />
+            </ButtonContainer>
+            <ButtonContainer>
+                P
+            </ButtonContainer>
+        </HeaderButton>
+
+        </Container>
+    )
+}
+
+export default Header;
